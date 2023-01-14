19a5bb0cf6e8bb9fa9db19c067c606f3e0df8d65
Skorin Vladislav <43670004+Skorin-Vladislav@users.noreply.github.com>
2023-01-14 13:29:54 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.scss";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  let [counter, setCounter] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="notifications">
+          <h2>Notifications</h2> <div className="counter">{counter}</div>
+        </div>
+        <p className="clear-notifications">Mark all as read</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
